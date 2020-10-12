fe9cce35f57734bfa5e43a634643c442b2b51d85
SethSterling <sethbdsterling@gmail.com>
2020-10-11 18:34:01 -0700

    Scaffolded all ASP.NET Identity pages

---
+++ CPW215JoesWebsiteSpeakingBits/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CPW215JoesWebsiteSpeakingBits.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CPW215JoesWebsiteSpeakingBits.Areas.Identity.IdentityHostingStartup))]
+namespace CPW215JoesWebsiteSpeakingBits.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
